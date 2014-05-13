199bbd1b3d863ac6b2d0e6f2ddee3637200b0344
Vivi_yd <vivid10400@gmail.com>
2014-05-13 11:56:41 +0000

    now added thrusting sound :D

--- InteractivePythonWeek7Project.py
+++ InteractivePythonWeek7Project.py
@@ -1,4 +1,4 @@
-# Implementation by Vivi.
+# program template for Spaceship
 import simplegui
 import math
 import random
@@ -7,11 +7,17 @@ import random
 WIDTH = 800
 HEIGHT = 600
 DIMENSION = [WIDTH, HEIGHT]
+ANGULAR_ACC = 0.25
 score = 0
 lives = 3
 time = 0.5
+#fraction of the  forward acceleration vector 
+FRACTION = 0.1
+#friction factor
+FRICTION = 0.02
 
-ANG_ACCN = 0.5
+#rock's angular accelration
+ROCK_ANGULAR_ACC = 0.04
 
 class ImageInfo:
     def __init__(self, center, size, radius = 0, lifespan = None, animated = False):
@@ -88,7 +94,7 @@ def dist(p,q):
 
 # Ship class
 class Ship:
-    def __init__(self, pos, vel, angle, image, info):
+    def __init__(self, pos, vel, angle , image, info):
         self.pos = [pos[0],pos[1]]
         self.vel = [vel[0],vel[1]]
         self.thrust = False
@@ -100,29 +106,39 @@ class Ship:
         self.radius = info.get_radius()
         
     def draw(self,canvas):
-        if self.thrust:
-            canvas.draw_image(self.image, [self.image_center[0] + self.image_size[0], self.image_center[0]],self.image_size, self.pos,self.image_size, self.angle )
-        else:    
-            canvas.draw_image(self.image, self.image_center, self.image_size, self.pos, self.image_size, self.angle)
-
+        if not self.thrust:
+            canvas.draw_image(self.image, self.image_center,self.image_size, self.pos,self.image_size, self.angle )
+        #if thrust is on this image should be displayed
+        else:
+            canvas.draw_image(self.image, [135, 45],self.image_size, self.pos,self.image_size, self.angle )
+                    
     def update(self):
-        #updating position with respect to velocity
+        #get the friction into play 
+        for i in range(2):
+            self.vel[i] = (1 - FRICTION) * self.vel[i]
+        
+        #get the forward vector (unit vector for acclerartion)
+        forwardVector = angle_to_vector(self.angle)
+        if self.thrust:
+            for i in range(2):
+                self.vel[i] += FRACTION * forwardVector[i]
+        
+        #update translation position of the ship 
         for i in range(2):
             self.pos[i] = (self.pos[i] +self.vel[i]) % DIMENSION[i]
-            
-        #updating angle with respect to angular velocity
-        self.angle += self.angle_vel
-        
-    def is_thrusting(self, thrusting):
-        
-        if thrusting:
+        #update angular position of the ship
+        self.angle += self.angle_vel        
+
+    def thrusting(self, boolean):
+        if boolean:
             self.thrust = True
+            ship_thrust_sound.play()
+            
         else:
             self.thrust = False
-     
-        
-    
-    
+            ship_thrust_sound.rewind()
+            
+
 # Sprite class
 class Sprite:
     def __init__(self, pos, vel, ang, ang_vel, image, info, sound = None):
@@ -142,10 +158,18 @@ class Sprite:
             sound.play()
    
     def draw(self, canvas):
-        canvas.draw_circle(self.pos, self.radius, 1, "Red", "Red")
+        canvas.draw_image(self.image, self.image_center,self.image_size, self.pos,self.image_size, self.angle )
     
     def update(self):
-        pass        
+        #update angular position of the sprite
+        self.angle += self.angle_vel
+        #update translation position of the sprite
+        for i in range(2):
+            self.pos[i] = (self.pos[i] +self.vel[i]) % DIMENSION[i]
+        
+        
+        
+                
 
            
 def draw(canvas):
@@ -172,31 +196,47 @@ def draw(canvas):
             
 # timer handler that spawns a rock    
 def rock_spawner():
-    pass
+    global time, a_rock
+    #random position
+    randomPos = [random.randrange(WIDTH), random.randrange(HEIGHT)]
+    #random velocity
+    randomVel = [0.03 * random.randrange(10, 50) * random.choice([1, -1]), 
+                 0.03 * random.randrange(10, 50) * random.choice([1, -1])]
+    #random angular velocity
+    randomAngularVel = 0.001 * random.randrange(20, 80) * random.choice([1, -1])
+    #create rock
+    #(self, pos, vel, ang, ang_vel, image, info, sound = None)
+    a_rock = Sprite(randomPos, randomVel, 0, randomAngularVel, asteroid_image, asteroid_info)
+    time = time + 1
+    
 
-# key down handlers
 
+#keydown event
 def keydown(key):
-    if key == simplegui.KEY_MAP["left"]:
-        my_ship.angle_vel -=  ANG_ACCN 
+    if key == simplegui.KEY_MAP['left']:
+        my_ship.angle_vel -=  ANGULAR_ACC 
 
-    elif key == simplegui.KEY_MAP["right"]:
-        my_ship.angle_vel +=  ANG_ACCN
+    elif key == simplegui.KEY_MAP['right']:
+        my_ship.angle_vel +=  ANGULAR_ACC
+
+    elif key == simplegui.KEY_MAP['up']:
+        my_ship.thrusting(True)
+
+    elif key == simplegui.KEY_MAP['down']:
+        my_ship.thrusting(False)
         
-    elif key == simplegui.KEY_MAP["up"]:
-        my_ship.is_thrusting(True)
-        
-# key up handlers
 
 def keyup(key):
     my_ship.angle_vel = 0
-    my_ship.is_thrusting(False)
+    my_ship.thrusting(False)
+    
+    
     
 # initialize frame
 frame = simplegui.create_frame("Asteroids", WIDTH, HEIGHT)
 
 # initialize ship and two sprites
-my_ship = Ship([WIDTH / 2, HEIGHT / 2], [1, 1], 0, ship_image, ship_info)
+my_ship = Ship([WIDTH / 2, HEIGHT / 2], [1, -0.5], 0, ship_image, ship_info)
 a_rock = Sprite([WIDTH / 3, HEIGHT / 3], [1, 1], 0, 0, asteroid_image, asteroid_info)
 a_missile = Sprite([2 * WIDTH / 3, 2 * HEIGHT / 3], [-1,1], 0, 0, missile_image, missile_info, missile_sound)
 
@@ -204,12 +244,8 @@ a_missile = Sprite([2 * WIDTH / 3, 2 * HEIGHT / 3], [-1,1], 0, 0, missile_image,
 frame.set_draw_handler(draw)
 frame.set_keydown_handler(keydown)
 frame.set_keyup_handler(keyup)
-
 timer = simplegui.create_timer(1000.0, rock_spawner)
 
 # get things rolling
 timer.start()
 frame.start()
-
-##Some print statement for testing
-
