639a85aef66f41e647bc4a08d4b8e86b49f7e9ad
Vivi_yd <vivid10400@gmail.com>
2014-06-12 06:25:33 +0000

    PracWeek0MiniProj.py first implementation, 7 failure out of 19 tests

---
+++ PracWeek0MiniProj.py
@@ -0,0 +1,84 @@
+
+class Solitaire_Mancala:
+    
+    """ a class for the game Solitaire Mancala """
+    
+    def __init__(self):
+        
+        """ initializing configuration """
+        
+        self.configuration = [0]
+                
+    def set_board(self, configuration):
+        
+        """set the board with the initialized configuration """
+        
+        self.configuration = configuration
+        
+    def __str__(self):
+        
+        """ reverses the configuration and covert to string """
+        
+        return str(self.configuration[::-1])
+        
+    def get_num_seeds(self, house_num):
+        
+        """ return number of seeds in the house of index house_num """
+        
+        return self.configuration[house_num]
+    
+    def is_legal_move(self, house_num):
+        
+        """ return whether move of house with index house_num is legal """
+        
+        return house_num > 0 and house_num == self.configuration[house_num]
+        
+        
+    def apply_move(self, house_num):
+        
+        """ apply the move if it's legal """
+        
+        if self.is_legal_move(house_num):
+            for i in self.configuration[:house_num]:
+                self.configuration[house_num] = 0
+                self.configuration[i] += 1
+                
+        return self.configuration
+    
+    def choose_move(self):
+        
+        """ return the legal move that is closest to the store """
+        
+        legal_move_index = []
+        for i in range(len(self.configuration)):
+            if self.is_legal_move(i):
+                legal_move_index.append(i)
+                return min(legal_move_index)
+            
+            return 0
+    
+    def is_game_won(self):
+        
+        """ declare winning of game if all houses are empty """
+        
+        for i in range(1, len(self.configuration)):
+            if self.get_num_seeds(i) != 0:
+                return False
+        return True
+    
+    def plan_moves(self):
+        
+        """ giving a list which is a series of legal moves of the whole game """
+        
+        moves = []
+        for i in range(1, len(self.configuration)):
+            if self.is_legal_move(i):
+                moves.append(i)
+                self.apply_move(i)
+        return moves
+    
+    
+import poc_mancala_testsuite                
+poc_mancala_testsuite.run_test(Solitaire_Mancala)
+import poc_mancala_gui
+poc_mancala_gui.run_gui(Solitaire_Mancala())
